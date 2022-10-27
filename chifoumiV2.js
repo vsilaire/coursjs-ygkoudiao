792def4dacc0505c56dbf9a78222ca3bf267a1b9
KOUADIO <yao-guillaume.kouadio@capgemini.com>
2022-10-27 15:36:39 +0200

    ChifoumiV2

--- chifoumiV2.js
+++ chifoumiV2.js
@@ -1,9 +1,9 @@
-function jeuOrdinateur() {
+function jeuOrdinateur(intervalle = Math.random()) {
     let jeuOrdinateur;
-    let choixOrdinateur = Math.random();
-    if (choixOrdinateur < 0.34 ) {
+    
+    if (intervalle < 0.34 ) {
         jeuOrdinateur = "pierre";
-    } else if(choixOrdinateur > 0.34 && choixOrdinateur < 0.66){
+    } else if(intervalle > 0.34 && intervalle < 0.66){
         jeuOrdinateur = "feuille";
     } else {
         jeuOrdinateur = "ciseaux";
@@ -11,36 +11,41 @@ function jeuOrdinateur() {
     console.log(jeuOrdinateur);
     return jeuOrdinateur;
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////////
 function jeuUser() {
     let jeuUser = prompt("Que jouez-vous? (pierre, feuille, ciseaux?)");
     console.log(jeuUser);
     return jeuUser;
 }
-
+//////////////////////////////////////////////////////////////////////////////////////////////
 let pointsOrdinateur = 0;
 let pointsJoueur =0;
 function choixVainqueurManche(jeuOrdinateur, jeuUser){
-    
+
+    // Victoires Ordinateur
     if (jeuOrdinateur === "pierre" && jeuUser === "ciseaux" 
     || jeuOrdinateur === "feuille" && jeuUser === "pierre" 
     || jeuOrdinateur === "ciseaux" && jeuUser === "feuille"
+    ////Pénalités triche ou vocabulaire user
     || jeuUser != "pierre" && jeuUser != "feuille" && jeuUser != "ciseaux"
     || jeuUser === "") {
         alert("Manche gagnée : Ordinateur")
         pointsOrdinateur ++;
         alert("Ordinateur : " + pointsOrdinateur + " - Joueur : " + pointsJoueur)
 
+    // Egalité
     } else if(jeuOrdinateur === jeuUser){
         alert("Egalité")
         alert("Ordinateur : " + pointsOrdinateur + " - Joueur : " + pointsJoueur)
+
+    // Victoire User
     } else{
         alert("Manche gagnée : Joueur")
         pointsJoueur ++;
         alert("Ordinateur : " + pointsOrdinateur + " - Joueur : " + pointsJoueur)
     }
 }
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
 let nbreManchesJouees;
 const manchesMax = 3;
 for (let nbreManchesJouees = 0; nbreManchesJouees < manchesMax; nbreManchesJouees++) {
@@ -53,3 +58,4 @@ if (pointsJoueur > pointsOrdinateur) {
 }else {
     alert("EGALITE, recommencez!")
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////
